f3899a538955fddd4b1d55c6ecc4b6516b0eaf7b
Joshua Pacheco <jp3032964444@gmail.com>
2023-05-16 14:02:51 -0700

    Implemented validation classes

--- translator2000.py
+++ translator2000.py
@@ -1,9 +1,10 @@
 #Assignment: Python Project 1
 #Joshua Pacheco (JP), 5/14/2023, The Translator Two Thousand, Python3 project
 #I am estimating this will take 4 hours to code. It actually took 6 hours, as I had issues with getting the URL entry to work and debugging
+
 # sys for bailing out of script runtime with exit method
 import sys
-# requests for acquiring internet resources for translatio 
+# requests for acquiring internet resources for translation 
 import requests
 # os library for validating local file existence
 import os
@@ -11,6 +12,7 @@ import os
 from textblob import TextBlob
 # set minimum text size to consider as valid text to translated
 MINIMUM_TEXT_LENGTH = 3
+MAX_CHOICE = 5
 TRANSLATED_LANGUAGE_FILE = 'selected_language.txt'
 SUPPORTED_LANGUAGES = {
 	'Spanish': 'es',
@@ -21,19 +23,45 @@ SUPPORTED_LANGUAGES = {
 	'Russian': 'ru',
     'Swahili' : 'sw',
 }
+class InputValidator(object):
+	def __init__(self, prompt_text):
+		self.prompt_text = prompt_text
+	def is_valid(self):
+		return False
+  
+class FileInputValidator(InputValidator):	
+     def is_valid(self):
+      return(len(self.prompt_text)>0 and os.path.exists(self.prompt_text))
+ 
+class TextInputValidator(InputValidator):
+    def is_valid(self):
+      return(len(self.prompt_text)>MINIMUM_TEXT_LENGTH)
+
+class URLInputValidator(InputValidator):
+    def is_valid(self):
+      return(self.prompt_text.startswith("http://") or self.prompt_text.startswith("https://"))
+    
+class LanguageInputValidator(InputValidator):
+    def is_valid(self):
+      return(self.prompt_text in SUPPORTED_LANGUAGES.keys())
+
+class MainMenuInputValidator(InputValidator):
+    def is_valid(self):
+        return(self.prompt_text.isdigit() and int(self.prompt_text) in range(MAX_CHOICE+1))
+
 # necessary functions
 def supported_languages_to_string():
     # convert dictionary to keys seperated by comma -> return string
 	return(", ".join(SUPPORTED_LANGUAGES.keys()))
 	# validate text size -> return true/false
 def validate_text_length(TEXT):
-	return(len(TEXT)>=MINIMUM_TEXT_LENGTH)
+    return(TextInputValidator(TEXT).is_valid())
 	# list supported textblob languages -> return list of language names
 def list_supported_languages():
 	return(SUPPORTED_LANGUAGES.keys())
 	# validate a language name -> return true/false
 def is_valid_language(LANGUAGE):
-	return(LANGUAGE in list_supported_languages())
+    return(LanguageInputValidator(LANGUAGE).is_valid())  
 def set_translated_language(LANGUAGE):
 	# record selected language to file -> return true/false
 	if not is_valid_language(LANGUAGE):
@@ -56,6 +84,8 @@ def translate_text(TEXT):
 	b = TextBlob(TEXT)
 	translated_text = b.translate(from_lang='en', to=DESTINATION_LANGUAGE_KEY)
 	print(f'Translation: "{translated_text}"')
+	with open("result.txt", "w") as f:
+		f.write(str(translated_text))
 	return(translated_text)
 def translate_url(URL):
 	# read text from uri -> return string
@@ -65,7 +95,7 @@ def translate_file(FILE_PATH):
 	# read text from file -> return string
     found = False
     while not found:
-      if  os.path.exists(FILE_PATH):
+      if FileInputValidator(FILE_PATH).is_valid():
         found = True
       else:
         FILE_PATH = input("Enter a valid file path: ")
@@ -75,7 +105,7 @@ def translate_file(FILE_PATH):
 def get_choice(max_choice):
     while True:
         choice = input("Enter your choice: ")
-        if choice.isdigit() and int(choice) in range(max_choice+1):
+        if MainMenuInputValidator(choice).is_valid():
             return int(choice)
         else:
             print("Invalid choice. Please enter a valid choice.")
@@ -88,7 +118,7 @@ def main():
         print("4. Translate text from local file")
         print("0. Exit")
 
-        choice = get_choice(5)
+        choice = get_choice(MAX_CHOICE)
 
         # Perform the selected task
         if choice == 1:
